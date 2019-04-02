2cdbf1c4599e9c141af42b79592cee5cbaaf6faf
Stepan <trofimovjava@gmail.com>
2019-04-02 16:09:52 +0300

    Define BaseCharacter

--- Source/Quidditch/Private/BaseCharacter.cpp
+++ Source/Quidditch/Private/BaseCharacter.cpp
@@ -4,5 +4,38 @@
 
 ABaseCharacter::ABaseCharacter()
 {
+	speed = 100.f;
+}
 
+void ABaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	FindMovementComponent();
+}
+
+void ABaseCharacter::FindMovementComponent()
+{
+	UPawnMovementComponent* movementComponent = GetMovementComponent();
+	bool isCharacterMovementComponent = movementComponent->IsA(UCharacterMovementComponent::StaticClass());
+	if (ensureMsgf(isCharacterMovementComponent, TEXT("Could not find UCharacterMovementComponent!")))
+	{
+		this->movementComponent = Cast<UCharacterMovementComponent>(movementComponent);
+	}
+}
+
+bool ABaseCharacter::CanBeAttacked() 
+{
+	return true;
+}
+
+FVector ABaseCharacter::GetLocation()
+{
+	return GetActorLocation();
+}
+
+void ABaseCharacter::AddDamage(FVector impulse)
+{
+	const FVector resultImpulse = impulse / movementComponent->Mass;
+	const bool changeVelocity = true;
+	movementComponent->AddImpulse(resultImpulse, changeVelocity);
 }
--- Source/Quidditch/Public/BaseCharacter.h
+++ Source/Quidditch/Public/BaseCharacter.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Character.h"
 #include "Target.h"
 #include "Hittable.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "BaseCharacter.generated.h"
 
 UCLASS(Abstract)
@@ -18,7 +19,7 @@ public:
 
 	//ITarget interface
 	virtual bool CanBeAttacked() override;
-	virtual void GetLocation() override;
+	virtual FVector GetLocation() override;
 
 	//IHittable interface
 	virtual void AddDamage(FVector impulse) override;
@@ -28,9 +29,17 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Character settings")
 	float speed;
 
+	UCharacterMovementComponent* movementComponent;
+
 	//Uncomment it when ETeamSide enum will exists
 	//TEnumAsByte<ETeamSide> team;
 
 	//Uncomment it when ETeamSide enum will exists
 	//TEnumAsByte<ERoleInTeam> role;
+
+	virtual void BeginPlay() override;
+
+private:
+
+	void FindMovementComponent();
 };
